77d23f0b741965054c330008bf9b740a19fd7d69
Clyde Autin <clyde.autin@gmail.com>
2024-03-13 09:58:05 -0700

    first commit

---
+++ test/mermaid_spec.rb
@@ -0,0 +1,27 @@
+# mermaid_spec.rb
+require 'rspec'
+require './lib/mermaid'
+
+describe Mermaid do
+  it 'is an instance of mermaid' do
+    mermaid = Mermaid.new('Sereia', 22)
+    expect(mermaid).to be_a Mermaid
+  end
+
+  it 'has a name' do
+    mermaid = Mermaid.new('Sereia', 22)
+    expect(mermaid.name).to eq 'Sereia'
+  end
+  
+  it 'has an age' do
+    mermaid = Mermaid.new('Sereia', 22)
+    expect(mermaid.age).to eq 22
+  end
+  
+  it 'can get older' do
+    mermaid = Mermaid.new('Sereia', 22)
+    mermaid.get_older
+    expect(mermaid.age).to eq 23
+  end
+end
+
